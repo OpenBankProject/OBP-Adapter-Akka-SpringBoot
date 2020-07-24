690248e6a1b066b631fc0c09b7da17d4f1ac6a8d
shuang <shuang@tesobe.com>
2020-07-24 17:17:07 +0800

    feature/make_akka_server_can_be_embed_model_start: add actorSystem as AkkaConfig's constructor parameter.

--- adapter-akka-commons/src/main/scala/com/openbankproject/adapter/akka/commons/config/AkkaConfig.scala
+++ adapter-akka-commons/src/main/scala/com/openbankproject/adapter/akka/commons/config/AkkaConfig.scala
@@ -11,14 +11,17 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 
-class AkkaConfig(outInBoundTransfer: OutInBoundTransfer) {
+case class AkkaConfig(outInBoundTransfer: OutInBoundTransfer, actorSystemOpt: Option[ActorSystem] = None) {
 
   val logger = LoggerFactory.getLogger(classOf[AkkaConfig])
   val SYSTEM_NAME = "SouthSideAkkaConnector_"
+
   val actorSystem: ActorSystem  = {
     val loadConfig = ConfigFactory.load
     val hostname = loadConfig.getString("akka.remote.netty.tcp.hostname")
-    val system = ActorSystem.create(SYSTEM_NAME + hostname.replace('.', '-'), loadConfig)
+    val system = actorSystemOpt.getOrElse {
+      ActorSystem.create(SYSTEM_NAME + hostname.replace('.', '-'), loadConfig)
+    }
     system.actorOf(Props.create(classOf[SouthSideActor], outInBoundTransfer), "akka-connector-actor")
 
     system
